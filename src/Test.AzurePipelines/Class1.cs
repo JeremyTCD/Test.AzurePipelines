0f854e6710a443088f117b783f5d4adaa0150db1
JeremyTCD <jeremytanchongdao@gmail.com>
2018-10-23 23:21:18 +0800

    Initial commit

---
+++ src/Test.AzurePipelines/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace Test.AzurePipelines
+{
+    public class Class1
+    {
+        public string ToLower(string message)
+        {
+            return message.ToLower();
+        }
+
+        public string ToUpper(string message)
+        {
+            return message.ToUpper();
+        }
+    }
+}
