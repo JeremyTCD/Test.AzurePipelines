9974010ca812765f9c83eb8aec1cfdce4ca61bc0
JeremyTCD <jeremytanchongdao@gmail.com>
2018-10-26 18:35:01 +0800

    Split test project up

---
+++ test/Test.AzurePipelines.ToLower.Tests/ToLowerTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Test.AzurePipelines.ToLower.Tests
+{
+    public class ToLowerTests
+    {
+        [Fact]
+        public void ToLower_ConvertsCharactersInMessageToLowercase()
+        {
+            var testSubject = new Class1();
+
+            string result = testSubject.ToLower("TEST");
+
+            Assert.Equal("test", result);
+        }
+    }
+}
---
+++ test/Test.AzurePipelines.ToUpper.Tests/ToUpperTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Test.AzurePipelines.ToUpper.Tests
+{
+    public class ToUpperTests
+    {
+        [Fact]
+        public void ToUpper_ConvertsCharactersInMessageToUppercase()
+        {
+            var testSubject = new Class1();
+
+            string result = testSubject.ToUpper("test");
+
+            Assert.Equal("TEST", result);
+        }
+    }
+}
